37ed48e5ad9ed2cb6e1266548ba26f582376ba44
danmastrow <15012375+danmastrow@users.noreply.github.com>
2025-01-07 18:09:24 +1100

    fix: build

--- style-guide-linter/src/app/api/lint/route.tsx
+++ style-guide-linter/src/app/api/lint/route.tsx
@@ -16,64 +16,64 @@ export type FeedbackItem = {
 
 const styleGuideFileName = "_style_guide_grammar.md";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
-async function generateResponse(
-  context: string,
-  prompt: string
-): Promise<string> {
-  const completion = await openai.chat.completions.create({
-    model: "gpt-4o",
-    messages: [
-      {
-        role: "system",
-        content: `You are a reviewer for the Australian Government Style Guide. You are reviewing a part of a document and providing feedback on the grammar punctuation and conventions, here is its contents '${context}'. Return feedback as json without MD formatting. Format the json as an array, where each object contains the following keys: 'relevantText', 'styleGuideRuleReferenceName', 'suggestion', 'comment'.  relevantText is the original part of the text that the feedback refers to. styleGuideRuleReferenceName is the name of the style guide rule that the feedback refers to. suggestion is the suggested change. comment is an optional field for additional comments. If there are no changes needed for a specific part of the text, don't append any.`,
-      },
-      { role: "user", content: prompt },
-    ],
-    temperature: 0,
-    max_tokens: 1000,
-  });
+// async function generateResponse(
+//   context: string,
+//   prompt: string
+// ): Promise<string> {
+//   const completion = await openai.chat.completions.create({
+//     model: "gpt-4o",
+//     messages: [
+//       {
+//         role: "system",
+//         content: `You are a reviewer for the Australian Government Style Guide. You are reviewing a part of a document and providing feedback on the grammar punctuation and conventions, here is its contents '${context}'. Return feedback as json without MD formatting. Format the json as an array, where each object contains the following keys: 'relevantText', 'styleGuideRuleReferenceName', 'suggestion', 'comment'.  relevantText is the original part of the text that the feedback refers to. styleGuideRuleReferenceName is the name of the style guide rule that the feedback refers to. suggestion is the suggested change. comment is an optional field for additional comments. If there are no changes needed for a specific part of the text, don't append any.`,
+//       },
+//       { role: "user", content: prompt },
+//     ],
+//     temperature: 0,
+//     max_tokens: 1000,
+//   });
 
-  return completion.choices[0].message.content ?? "";
-}
-async function getStyleGuidance(userInput: string) {
-  const dataPath = path.join(
-    process.cwd(),
-    "src",
-    "app",
-    "data",
-    styleGuideFileName
-  );
+//   return completion.choices[0].message.content ?? "";
+// }
+// async function getStyleGuidance(userInput: string) {
+//   const dataPath = path.join(
+//     process.cwd(),
+//     "src",
+//     "app",
+//     "data",
+//     styleGuideFileName
+//   );
 
-  const fileContents = fs.readFileSync(dataPath, "utf-8");
+//   const fileContents = fs.readFileSync(dataPath, "utf-8");
 
-  const gptResponse = await generateResponse(fileContents, userInput);
+//   const gptResponse = await generateResponse(fileContents, userInput);
 
-  return gptResponse;
-}
+//   return gptResponse;
+// }
 
 export async function POST(req: NextRequest) {
   // No longer active.
   return new NextResponse(null, { status: 400 });
 
-  try {
-    if (!req.body) {
-    }
+  // try {
+  //   if (!req.body) {
+  //   }
 
-    const request: LintRequest = await req.json();
+  //   const request: LintRequest = await req.json();
 
-    if (request.content === "") {
-      return new NextResponse(null, { status: 400 });
-    }
+  //   if (request.content === "") {
+  //     return new NextResponse(null, { status: 400 });
+  //   }
 
-    const response = await getStyleGuidance(request.content);
-    const responseAsJson = JSON.parse(response) ?? [];
-    return NextResponse.json(responseAsJson);
-  } catch (error) {
-    console.error("Error linting content:", error);
-    return new NextResponse(null, { status: 500 });
-  }
+  //   const response = await getStyleGuidance(request.content);
+  //   const responseAsJson = JSON.parse(response) ?? [];
+  //   return NextResponse.json(responseAsJson);
+  // } catch (error) {
+  //   console.error("Error linting content:", error);
+  //   return new NextResponse(null, { status: 500 });
+  // }
 }
